baa1d7899ef6769f67425af12bd9270bd6689cc2
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2025-08-19 18:57:19 -0300

    Incrementa Introducao e FundamentacaoTeorica

--- compilar.sh
+++ compilar.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/bash
+
+# pedidos de citação
+/usr/bin/pdflatex -output-directory=build ./Principal.tex;
+
+# construção de bibliografia bibtex
+/usr/bin/bibtex ./build/Principal.aux;
+
+# resolução de referências cruzadas
+/usr/bin/pdflatex -output-directory=build ./Principal.tex;
 /usr/bin/pdflatex -output-directory=build ./Principal.tex;
